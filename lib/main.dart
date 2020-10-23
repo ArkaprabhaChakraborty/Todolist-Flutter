0becc2c6ad32b4316be1c7f4f02de4c3927bae70
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2020-10-23 16:56:25 +0530

    Final

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
@@ -17,80 +21,88 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List todos = List();
   String input = "";
-  @override
-  void initState() {
-    super.initState();
-    todos.add("Item1");
-    todos.add("Item2");
-    todos.add("Item3");
-    todos.add("Item4");
-    todos.add("Item5");
+  createTodos(input) {
+    DocumentReference documentReference =
+        FirebaseFirestore.instance.collection("MyToDos").doc(input);
+    Map<String, String> todos = {"todotitle": input};
+    documentReference.set(todos).whenComplete(() {
+      print("$input created");
+    });
+  }
+
+  deleteTodos(input) {
+    DocumentReference documentReference =
+        FirebaseFirestore.instance.collection("MyToDos").doc(input);
+    documentReference.delete().whenComplete(() {
+      print("$input deleted");
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("My To-Do's"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text("Add to List"),
-                  content: TextField(
-                    onChanged: (String value) {
-                      input = value;
-                    },
-                  ),
-                  actions: <Widget>[
-                    FlatButton(
-                        onPressed: () {
-                          setState(() {
-                            todos.add(input);
-                          });
-                          Navigator.of(context).pop();
-                        },
-                        child: Text("Add"))
-                  ],
-                );
-              });
-        },
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
+        appBar: AppBar(
+          title: Text("My To-Do's"),
         ),
-      ),
-      body: ListView.builder(
-          itemCount: todos.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Dismissible(
-              key: UniqueKey(),
-              child: Card(
-                elevation: 4,
-                margin: EdgeInsets.all(8),
-                shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-                child: ListTile(
-                title: Text(todos[index]),
-                trailing: IconButton(
-                    icon: Icon(Icons.delete, color: Colors.red),
-                    onPressed: () {
-                      setState(() {
-                        todos.removeAt(index);
-                      });
-                    }),
-              )),
-              onDismissed: (DismissDirection right) {
-                setState(() {
-                  todos.removeAt(index);
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text("Add to List"),
+                    content: TextField(
+                      onChanged: (String value) {
+                        input = value;
+                      },
+                    ),
+                    actions: <Widget>[
+                      FlatButton(
+                          onPressed: () {
+                            createTodos(input);
+                            Navigator.of(context).pop();
+                          },
+                          child: Text("Add"))
+                    ],
+                  );
                 });
-              },
-            );
-          }),
-    );
+          },
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+        ),
+        body: StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection("MyToDos").snapshots(),
+            builder: (context, snapshots) {
+              if (snapshots.data == null) return CircularProgressIndicator();
+              return ListView.builder(
+                  itemCount: snapshots.data.documents.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot documentSnapshot =
+                        snapshots.data.documents[index];
+                    return Dismissible(
+                      key: UniqueKey(),
+                      child: Card(
+                          elevation: 4,
+                          margin: EdgeInsets.all(8),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)),
+                          child: ListTile(
+                            title: Text(documentSnapshot["todotitle"]),
+                            trailing: IconButton(
+                                icon: Icon(Icons.delete, color: Colors.red),
+                                onPressed: () {
+                                  deleteTodos(documentSnapshot['todotitle']);
+                                }),
+                          )),
+                      onDismissed: (DismissDirection right) {
+                        deleteTodos(documentSnapshot['todotitle']);
+                      },
+                    );
+                  });
+            }));
   }
 }
